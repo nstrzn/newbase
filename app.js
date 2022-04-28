0fb869f651b59bf2eda680035995f9592ec53d88
ToniRahelK <92578082+ToniRahelK@users.noreply.github.com>
2022-04-28 22:32:48 +0200

    6

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const homeController = require("./controllers/homeController");
-const port = 3000;
-var app = express();
+const routeResponseMap = {
+    "/info": "<h1>Info Page</h1>",
+    "/contact": "<h1>Contact Us</h1>",
+    "/about": "<h1>Learn More About Us.</h1>",
+    "/hello": "<h1>Say hello by emailing us here</h1>",
+    "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
+    };
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+if (routeResponseMap[req.url]) {
+res.end(routeResponseMap[req.url]);
+} else {
+res.end("<h1>Welcome!</h1>");
+}
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    console.log(`request made to: ${req.url}`);
-    next();
+const port = 3001,
+http = require("http"),
+httpStatus = require("http-status-codes"),
+app = http.createServer((request, response) => {
+console.log("Received an incoming request!");
+response.writeHead(httpStatus.OK, {
+"Content-Type": "text/html"
 });
 
-app.post("/", homeController.logRequestPaths);
+app.on("request", (req, res) => {
+    res.writeHead(httpStatus.OK, {
+    "Content-Type": "text/html"
+    });
+    let responseMessage = "<h1>This will show on the screen.</h1>";
+    res.end(responseMessage);
+    });
 
-app.get("/", homeController.getHomePage);
-
-app.post("/contact", (req, res) => {
-    res.send("Contact information submitted");
+app.on("request", (req, res) => {
+var body = [];
+req.on("data", (bodyData) => {
+body.push(bodyData);
 });
-
-app.get("/users/:id", homeController.sendReqParam);
-
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+req.on("end", () => {
+body = Buffer.concat(body).toString();
+console.log(`Request Body Contents: ${body}`);
 });
+console.log(`Method: ${getJSONString(req.method)}`);
+console.log(`URL: ${getJSONString(req.url)}`);
+console.log(`Headers: ${getJSONString(req.headers)}`);
+
+let responseMessage = "<h1>Hello, Universe!</h1>";
+response.write(responseMessage);
+response.end();
+console.log(`Sent a response : ${responseMessage}`);
+});
+app.listen(port);
+console.log(`The server has started and is listening on port number:
+➥ ${port}`);
+
+const getJSONString = obj => {
+    return JSON.stringify(obj, null, 2);
+};
