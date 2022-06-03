7d6e051db87d626e2bd78b1bb7f17a23312ab214
ToniRahelK <92578082+ToniRahelK@users.noreply.github.com>
2022-06-03 13:33:52 +0200

    subscriber

--- models/Subscriber.js
+++ models/Subscriber.js
@@ -1,7 +1,27 @@
-const mongoose = require("mongoose"),
-subscriberSchema = mongoose.Schema({
-name: String,
-email: String,
-zipCode: Number
+const mongoose = require("mongoose");
+const subscriberSchema = new mongoose.Schema({
+name: {
+type: String,
+required: true
+},
+email: {
+type: String,
+required: true,
+lowercase: true,
+unique: true
+},
+zipCode: {
+type: Number,
+min: [10000, "Zip code too short"],
+max: 99999
+}
 });
-module.exports = mongoose.model("Subscriber", subscriberSchema);
+subscriberSchema.methods.getInfo = function() {
+  return `Name: ${this.name} Email: ${this.email} Zip Code:
+  ➥ ${this.zipCode}`;
+  };
+  subscriberSchema.methods.findLocalSubscribers = function() {
+  return this.model("Subscriber")
+  .find({zipCode: this.zipCode})
+.exec();
+};
