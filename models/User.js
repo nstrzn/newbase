bb202b162dcb1bc8c9a6cbc9f646e75061422136
fadooxx <51059277+fadooxx@users.noreply.github.com>
2022-06-08 20:41:13 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,62 +1,40 @@
-"use strict";
-
 const mongoose = require("mongoose"),
-  { Schema } = require("mongoose"),
-
-var userSchema = new Schema(
-  {
-    name: {
-      first: {
-        type: String,
-        trim: true
+  { Schema } = mongoose,
+  userSchema = new Schema(
+    {
+      name: {
+        first: {
+          type: String,
+          trim: true,
+        },
+        last: {
+          type: String,
+          trim: true,
+        },
       },
-      last: {
+      email: {
         type: String,
-        trim: true
-      }
+        required: true,
+        lowercase: true,
+        unique: true,
+      },
+      zipCode: {
+        type: Number,
+        min: [1000, "Zip code too short"],
+        max: 99999,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      channels: [{ type: Schema.Types.ObjectId, ref: "Channel" }],
+      subscribedAccount: { type: Schema.Types.ObjectId, ref: "Subscriber" },
     },
-    email: {
-      type: String,
-      required: true,
-      lowercase: true,
-      unique: true
-    },
-    zipCode: {
-      type: Number,
-      min: [1000, "Zip code too short"],
-      max: 99999
-    },
-    password: {
-      type: String,
-      required: true
-    },
-  },
-  {
-    timestamps: true
-  }
-);
-
-userSchema.virtual("fullName").get(function() {
+    {
+      timestamps: true,
+    }
+  );
+userSchema.virtual("fullName").get(function () {
   return `${this.name.first} ${this.name.last}`;
 });
-
-userSchema.pre("save", function(next) {
-  let user = this;
-  if (user.subscribedAccount === undefined) {
-    Subscriber.findOne({
-      email: user.email
-    })
-      .then(subscriber => {
-        user.subscribedAccount = subscriber;
-        next();
-      })
-      .catch(error => {
-        console.log(`Error in connecting subscriber: ${error.message}`);
-        next(error);
-      });
-  } else {
-    next();
-  }
-});
-
 module.exports = mongoose.model("User", userSchema);
