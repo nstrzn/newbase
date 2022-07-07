8d0a045036b8674fb2bb04193fc1a2405a8ddcf4
AntjeSt <82602132+AntjeSt@users.noreply.github.com>
2022-07-07 15:59:59 +0200

    jsonwebtoken

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,11 @@
 "use strict";
 
+const usersController = require("../controllers/usersController");
 const router = require("express").Router(),
 themesController = require("../controllers/themesController");
+
+router.post("/login", usersController.apiAuthenticate);
+router.use(usersController.verifyJWT);
 router.get("/forum", themesController.index, themesController.filterUserThemes, themesController.respondJSON);
 router.get("/forum/:id/join", themesController.join, themesController.respondJSON);
 router.use(themesController.errorJSON);
