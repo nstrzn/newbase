adb2fc4fbcebfe8c57dafd0e040c25174351f310
fadooxx <51059277+fadooxx@users.noreply.github.com>
2022-07-12 19:01:38 +0200

    Story 011 - User kann andere Mitarbeiter über den Chat kontaktieren

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -10,5 +10,8 @@ module.exports = {
   logRequestPaths: (req, res, next) => {
     console.log(`request made to: ${req.url}`);
     next();
+  },
+  chat: (req, res) => {
+    res.render("chat");
   }
 };
--- main.js
+++ main.js
@@ -72,6 +72,8 @@ app.get("/", (req, res) => {
 });
 app.use("/", router);
 
-app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
-});
+}),
+io = require("socket.io")(server);
+require("./controllers/chatController")(io);
